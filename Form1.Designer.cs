7e6be6fe232d4e4fe1d691444a800c8bdfa3a55d
Kokosik347 <ganzhakgtl@gmail.com>
2024-06-02 19:43:48 +0300

    Finish Task number 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.bStart = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(346, 228);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(334, 180);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 22);
+            this.tbInput.TabIndex = 1;
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(821, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.bStart);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tClock;
     }
 }
 
